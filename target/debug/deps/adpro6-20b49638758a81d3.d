20928bac0138125c5fc7283c9e738ec633a75c3e
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-20 09:27:02 +0700

    feat: init repo

---
+++ target/debug/deps/adpro6-20b49638758a81d3.d
@@ -0,0 +1,5 @@
+C:\Users\OMEN\Desktop\codes\adpro6\target\debug\deps\libadpro6-20b49638758a81d3.rmeta: src\main.rs
+
+C:\Users\OMEN\Desktop\codes\adpro6\target\debug\deps\adpro6-20b49638758a81d3.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/adpro6-acbb5ed4f799c67f.d
@@ -0,0 +1,5 @@
+C:\Users\OMEN\Desktop\codes\adpro6\target\debug\deps\libadpro6-acbb5ed4f799c67f.rmeta: src\main.rs
+
+C:\Users\OMEN\Desktop\codes\adpro6\target\debug\deps\adpro6-acbb5ed4f799c67f.d: src\main.rs
+
+src\main.rs:
